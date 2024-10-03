40d976293d50297faf42af6173575741edd11097
VitaminNeedsHelp <baylun20@htl-kaindorf.at>
2024-10-03 12:15:09 +0200

    Features: Min, Max, Avg

--- src/Uebungen/ue00_lists.py
+++ src/Uebungen/ue00_lists.py
@@ -3,3 +3,6 @@ import random
 numbers = [random.randint(-10, 10) for i in range(10)]
 print(f"Zufällige Zahlen: {numbers}")
 print(f"Summe: {sum(numbers)}")
+print(f"Minimum: {min(numbers)}")
+print(f"Maximum: {max(numbers)}")
+print(f"Mittlerer Wert: {sum(numbers) / len(numbers)}")
